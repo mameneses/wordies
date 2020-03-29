2f568cc1aea3673670a9ecaff305bd50f0099932
Matias Meneses <matiasameneses@gmail.com>
2020-03-29 11:14:35 -0700

    Updates

---
+++ precache-manifest.b670f547d2dadb74b46356d48005557a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98dbc119b4d6c132a14d49d8090922e3",
+    "url": "/wordies/index.html"
+  },
+  {
+    "revision": "1dbafc1d7b354c39c317",
+    "url": "/wordies/static/css/main.fa6d27bd.chunk.css"
+  },
+  {
+    "revision": "3c8d8d7bd6fc561799b9",
+    "url": "/wordies/static/js/2.7967d98c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wordies/static/js/2.7967d98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbafc1d7b354c39c317",
+    "url": "/wordies/static/js/main.98698dd0.chunk.js"
+  },
+  {
+    "revision": "ded3393907024d8a56cf",
+    "url": "/wordies/static/js/runtime-main.9152eb6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wordies/static/media/logo.5d5d9eef.svg"
+  }
+]);
